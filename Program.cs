d0cd3a9a62a5a04efe628596c50f5dfb0d39e0a9
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2024-04-24 11:44:23 -0300

    Add Exercicio 13

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicios
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Exercicio1 a = new Exercicio1();
+            a.exercicio1();
+        }
+    }
+}
