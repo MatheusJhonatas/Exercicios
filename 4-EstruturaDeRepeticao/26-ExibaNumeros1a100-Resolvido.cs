c1f78bd277d8da997b6b3b8e2326cc4b41f43c02
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2024-05-10 13:06:59 -0300

    Add Exercicio 26

---
+++ 4-EstruturaDeRepeticao/26-ExibaNumeros1a100-Resolvido.cs
@@ -0,0 +1,20 @@
+/*26) Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+Meu código:*/
+
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            for (var i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
