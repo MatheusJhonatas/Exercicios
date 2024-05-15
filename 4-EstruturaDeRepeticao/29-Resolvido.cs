5aa62ab87a6e5c5f40e6888a16835b692130f629
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2024-05-15 18:11:30 -0300

    Add exercicio 29

---
+++ 4-EstruturaDeRepeticao/29-Resolvido.cs
@@ -0,0 +1,21 @@
+/*29) Faça um programa que exiba os números de 1 até 2000
+Meu código: */
+
+using System;
+using System.Runtime.InteropServices;
+namespace Exercicio29
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            for (var i = 1; i <= 2000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
+
